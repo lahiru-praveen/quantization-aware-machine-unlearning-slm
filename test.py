b1cb37737fc2fba5410f3f8bbd38e4e8630d4a5c
Lahiru <lahirupraveen43@gmail.com>
2026-08-08 18:15:10 +0530

    Stable version. Ready for the demonstration

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_json("C:/Users/Asus/Documents/GitHub/quantization-aware-machine-unlearning-slm/data/raw/retain2.json")
-df.to_csv("retain_set2.csv", index=False)
-
-print("JSON has been converted to CSV successfully!")
